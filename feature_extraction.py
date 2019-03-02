cdefee2e06ba52ed7fd02a56370345dbfc16c252
akshu <akshay.suryanarayan@gmail.com>
2019-03-02 13:59:53 +0530

    feature extraction code for sample song

---
+++ feature_extraction.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+"""
+Author: Akshay
+Date: 14th oct 2018
+Time: 5:00am
+Obj: to try to get features for inbuilt song dataset
+source :
+"""
+
+from __future__ import print_function
+import librosa
+
+# 1. Get the file path to the included audio example
+filename = librosa.util.example_audio_file()
+
+# 2. Load the audio as a waveform `y`
+#    Store the sampling rate as `sr`
+y, sr = librosa.load(filename)
+y = librosa.effects.harmonic(y)
+tonnetz = librosa.feature.tonnetz(y=y, sr=sr)
+#print("tonnetz: ",tonnetz)
+
+import soundfile as sf
+
+X, sample_rate = sf.read(filename, dtype='float32')
+if X.ndim > 1:
+        X = X[:,0]
+        X = X.T
+        
+import numpy as np
+
+tonnetz = np.mean(librosa.feature.tonnetz(y=y, sr=sr))
+print("tonnetz: ", tonnetz)
+print("______________________________________________________")
+
+
+
+mfccs = np.mean(librosa.feature.mfcc(y=y, sr=sr))
+print("mfcc: ",mfccs)
+print("______________________________________________________")
+
+
+
+#stft = np.abs(librosa.stft(X))
+chroma = np.mean(librosa.feature.chroma_stft(y=y, sr=sr))
+print("chroma: ",chroma)
+print("______________________________________________________")
+
+
+
+mel = np.mean(librosa.feature.melspectrogram(y=y, sr=sr))
+print("mel: ",mel)
+print("______________________________________________________")
+
+
+
+contrast = np.mean(librosa.feature.spectral_contrast(y=y,sr=sr))
+print("contrast: ",contrast)
+print("______________________________________________________")
+
